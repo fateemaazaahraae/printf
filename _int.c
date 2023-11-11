d4990759efecb581dd1f9f2908e766b748eb8ddd
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-11 09:12:42 -0800

    pushinng

---
+++ _int.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+int main()
+{
+	char c = 'a';
+	char *s = "tiima";
+	_printf("hello my name is tiim%c", c);
+	printf("\n");
+	_printf("hello my name is %s", "tiima");
+	printf("\n");
+}
