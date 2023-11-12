c0ab6ab590a731a28faf11bac26969f7b9640ee9
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-12 01:59:14 -0800

    push

--- _printf.c
+++ _printf.c
@@ -11,9 +11,11 @@
 int _printf(const char *format, ...)
 {
 	b spe[] = {
-		{"%s", print_string}, {"%c", print_char}, {"%i", print_int}, {"%d", print_d}, {"%%", print_sign},
-		{"%b", print_binary}, {"%u", print_unsigned}, {"%o", print_octal}, {"%x", print_hexa_low}, {"%X", print_hexa_upp},
-		{"%S", print_String}, {"%p", print_add}, {"%r", print_rev_str}, {"%R", print_ro13}
+		{"%s", print_string}, {"%c", print_char}, {"%i", print_int},
+		{"%d", print_d}, {"%%", print_sign}, {"%b", print_binary},
+		{"%u", print_unsigned}, {"%o", print_octal}, {"%x", print_hexa_low},
+		{"%X", print_hexa_upp}, {"%S", print_String}, {"%p", print_add},
+		{"%r", print_rev_str}, {"%R", print_ro13}
 	};
 	va_list args;
 	int i;
@@ -23,13 +25,13 @@ int _printf(const char *format, ...)
 	i = 0;
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-			return (-1);
+		return (-1);
 	while (format[i] != '\0')
 	{
 		j = 0;
 		while (j <= 13 && format[i] == spe[j].specefier[0])
 		{
-			if (format [i + 1] == spe[j].specefier[1])
+			if (format[i + 1] == spe[j].specefier[1])
 			{
 				len += spe[j].f(args);
 				i = i + 2;
