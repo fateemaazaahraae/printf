073f719ae3f9cd3e1d1cc79dc9baae7264d6bf4e
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-12 01:47:09 -0800

    task 0

---
+++ ft_strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * ft_strlen - calculate the length
+ * @str: string
+ *
+ * Return: the length of str
+ */
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
---
+++ print_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_string - printing a given string
+ * @list: list of argument
+ *
+ * Return: the length of the string
+ */
+
+int print_string(va_list list)
+{
+	char *str;
+	int i, length;
+
+	str = va_arg(list, char *);
+	i = 0;
+	if (str == NULL)
+	{
+		str = "(null)";
+		length = ft_strlen(str);
+		while (i < length)
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		length = ft_strlen(str);
+		while (i < length)
+		{
+			ft_putchar(str[i])
+				i++;
+		}
+	}
+	return (length);
+}
