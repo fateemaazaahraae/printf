efb1d76319777097f95611c15214399174cfaeec
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-13 11:56:41 -0800

    print_bit

--- print_bit.c
+++ print_bit.c
@@ -1,25 +1,53 @@
 #include "main.h"
 
 /**
- * print_bit - this function print int in binary
- * @list: list of arguments
+ * bit_len - number of digits of the binary representation of a number.
+ * @num: thr number.
  *
- * Return: length
+ * Return: the number of a digits.
  */
-
-int print_bit(va_list list)
+int bit_len(unsigned int num)
 {
-	unsigned int octet;
-	unsigned int i;
-	int length;
+	int len = 0;
 
-	i = 128;
-	length = 0;
-	octet = va_arg(list, unsigned int);
-	while (i >= 1)
+	while (num != 0)
 	{
-		length += ft_putchar((!!(octet & i)) + '0');
-		i = i >> 1;
+		len++;
+		num /= 2;
 	}
-	return (length);
+	return (len);
+}
+
+/**
+ * put_bit - print decimal number in binary.
+ * @num: the number.
+ */
+void put_bit(unsigned int num)
+{
+	if (num == 0)
+		return;
+	put_bit(num / 2);
+	_putchar(num % 2 + '0');
+}
+
+/**
+ * print_bit - print decimal number in binary.
+ * @args: list of arguments (to extract the num from it).
+ *
+ * Return: the number of digits.
+ */
+int print_bit(va_list args)
+{
+	unsigned int num;
+	int len;
+
+	num = va_arg(args, int);
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	put_bit(num);
+	len = bit_len(num);
+	return (len);
 }
